da0cb29be08f08027126f956ae38f1bd8424153e
Joao <joaoo.c@hotmail.com>
2022-10-17 14:35:17 +0100

    Adicionando carousel.module

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,48 +2,45 @@ import Carousel from 'react-bootstrap/Carousel';
 import Carousel1 from '../../images/carousel1.png'
 import Carousel2 from '../../images/carousel2.png'
 import Carousel3 from '../../images/carousel3.png'
-import '../carousel/carousel.css'
+import Styles from '../carousel/carousel.module.css'
 import React  from 'react';
 
 function CarouselFade() {
   return (
-    <Carousel fade interval={2000} className='carousel-layout'>
-      <Carousel.Item>
+    <Carousel fade interval={2000} className={Styles.carousel_layout}>
+      <Carousel.Item className={Styles.carousel_item}>
         <img
-            id='carousel-img'
           className="carousel-img d-block w-100"
           src={Carousel1}
           alt="First slide"
         />
-        <Carousel.Caption className='titulo-carousel'>
-          <h1 id='main-title'>Quase 10 anos realizando sonhos.</h1>
-          <p id='main-p'>Possuimos os melhores profissionais para trazer o melhor aos nossos clientes.</p>
+        <Carousel.Caption className={Styles.titulo_carousel}>
+          <h1 className={Styles.main_title}>Quase 10 anos realizando sonhos.</h1>
+          <p className={Styles.main_p}>Possuimos os melhores profissionais para trazer o melhor aos nossos clientes.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className={Styles.carousel_item}>
         <img
-            id='carousel-img'
           className=" d-block w-100"
           src={Carousel2}
           alt="Second slide"
         />
 
-        <Carousel.Caption className='titulo-carousel'>
-          <h1 id='main-title'>Ganhe mais produtividade.</h1>
-          <p id='main-p'>Reconstruimos se necessario todo o processo da sua empresa.</p>
+        <Carousel.Caption className={Styles.titulo_carousel}>
+          <h1 className={Styles.main_title}>Ganhe mais produtividade.</h1>
+          <p className={Styles.main_p}>Reconstruimos se necessario todo o processo da sua empresa.</p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
+      <Carousel.Item className={Styles.carousel_item}>
         <img
-            id='carousel-img'
           className=" d-block w-100"
           src={Carousel3}
           alt="Third slide"
         />
 
-        <Carousel.Caption className='titulo-carousel'>
-          <h1 id='main-title'>Comprometimento e Responsabilidade.</h1>
-          <p id='main-p'>
+        <Carousel.Caption className={Styles.titulo_carousel}>
+          <h1 className={Styles.main_title}>Comprometimento e Responsabilidade.</h1>
+          <p className={Styles.main_p}>
             Deixe sua empresa nas maos de quem entende.
           </p>
         </Carousel.Caption>
